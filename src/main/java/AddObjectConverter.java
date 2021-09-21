c38e170af7316d68d73749e35bab5c2f314da0c5
QQ喵 <gqqnb2005@gmail.com>
2021-09-21 22:11:28 +0800

    - Add method loadList - Add import

--- src/main/java/AddObjectConverter.java
+++ src/main/java/AddObjectConverter.java
@@ -1,7 +1,9 @@
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,6 +11,7 @@ public class AddObjectConverter extends JavaParserBaseVisitor<Object> {
 	final static Pattern methodNamePattern = Pattern.compile("add(\\w+)Object");
 
 	final TokenStreamRewriter rewriter;
+	boolean hasLoadList = false;
 
 	public AddObjectConverter(TokenStreamRewriter rewriter) {
 		this.rewriter = rewriter;
@@ -47,9 +50,80 @@ public class AddObjectConverter extends JavaParserBaseVisitor<Object> {
 			}
 		}
 
-//		return super.visitMethodDeclaration(ctx);
+		if ("loadList".equals(methodName))
+			hasLoadList = true;
+
 		return null;
 	}
+
+	HashSet<String> imports = new HashSet<>(Arrays.asList(
+			"com.owlike.genson.Genson",
+			"org.hyperledger.fabric.shim.ChaincodeStub"));
+
+
+	@Override
+	public Object visitImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
+		String name = ctx.qualifiedName().getText();
+		imports.remove(name);
+
+		return null;
+	}
+
+	Token typeStartToken = null;
+
+	@Override
+	public Object visitTypeDeclaration(JavaParser.TypeDeclarationContext ctx) {
+		if (typeStartToken == null)
+			typeStartToken = ctx.start;
+
+		return super.visitTypeDeclaration(ctx);
+	}
+
+	@Override
+	public Object visitClassBody(JavaParser.ClassBodyContext ctx) {
+		super.visitClassBody(ctx);
+
+
+		if (hasLoadList == false) {
+			String line = String.join("\n", loadList) + "\n";
+			rewriter.insertBefore(ctx.stop, line);
+
+			System.out.println("loadList method added");
+		}
+		return null;
+	}
+
+	@Override
+	public Object visitCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+		super.visitCompilationUnit(ctx);
+
+		if (typeStartToken != null) {
+			if (imports.size() > 0) {
+				rewriter.insertBefore(typeStartToken, "\n");
+				for (String name : imports)
+					rewriter.insertBefore(typeStartToken, "import " + name + ";\n");
+			}
+		}
+
+		return null;
+	}
+
+	private static ArrayList<String> loadList = new ArrayList<>(Arrays.asList(
+			"	private static <T> List<T> loadList(Class<T> clazz) {",
+			"		String key = clazz.getSimpleName();",
+			"		List<T> list = AllInstance.get(key);",
+			"		if (list == null || list.size() == 0) {",
+			"			String json = stub.getStringState(key);",
+			"			System.out.printf(\"loadList %s: %s\\n\", key, json);",
+			"			if (json != null && Objects.equals(json, \"\") == false)",
+			"				list = GensonHelper.deserializeList(genson, json, clazz);",
+			"			else",
+			"				list = new LinkedList<>();",
+			"			AllInstance.put(key, list);",
+			"		}",
+			"		return list;",
+			"	}"
+	));
 
 
 }
