bf46aa49779c3d7a449eace2831ac8115dd07cac
QQ喵 <gqqnb2005@gmail.com>
2021-11-07 16:02:02 +0800

    Class and interface modifiers are different from general modifiers

--- src/main/java/rm2hyperledger/checkers/ModifierChecker.java
+++ src/main/java/rm2hyperledger/checkers/ModifierChecker.java
@@ -1,5 +1,6 @@
 package rm2hyperledger.checkers;
 
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 import rm2hyperledger.JavaParser;
 
@@ -12,9 +13,28 @@ public class ModifierChecker {
 	 * @param token JavaParser tokens, e.g., JavaParser.PUBLIC
 	 * @return
 	 */
-	public static boolean hasModifier(List<JavaParser.ClassOrInterfaceModifierContext> ctx, int token) {
+	public static <T> boolean hasCIModifier(List<JavaParser.ClassOrInterfaceModifierContext> ctx, int token) {
 		for (JavaParser.ClassOrInterfaceModifierContext modifier : ctx) {
-			if ((((TerminalNodeImpl) modifier.children.get(0)).symbol).getType() == token)
+			ParseTree t = modifier.children.get(0);
+			if (t instanceof TerminalNodeImpl && (((TerminalNodeImpl) t).symbol).getType() == token)
+				return true;
+		}
+		return false;
+
+	}
+
+	/**
+	 * @param ctx
+	 * @param token JavaParser tokens, e.g., JavaParser.PUBLIC
+	 * @return
+	 */
+	public static boolean hasModifier(List<JavaParser.ModifierContext> ctx, int token) {
+		for (var modifier : ctx) {
+			ParseTree t = modifier.children.get(0);
+			if (t instanceof TerminalNodeImpl && (((TerminalNodeImpl) t).symbol).getType() == token)
+				return true;
+			t = t.getChild(0);
+			if (t instanceof TerminalNodeImpl && (((TerminalNodeImpl) t).symbol).getType() == token)
 				return true;
 		}
 		return false;
