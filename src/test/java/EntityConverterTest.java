9768fd7e79e160f1348bec0045ac41c2ff434420
QQ喵 <gqqnb2005@gmail.com>
2021-11-07 16:04:07 +0800

    Add EntityConverterTest

---
+++ src/test/java/EntityConverterTest.java
@@ -0,0 +1,26 @@
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+import org.junit.jupiter.api.Test;
+import rm2hyperledger.EntityConverter;
+import rm2hyperledger.JavaLexer;
+import rm2hyperledger.JavaParser;
+
+import java.io.IOException;
+
+public class EntityConverterTest {
+
+	@Test
+	void test() throws IOException {
+		var tokens = new CommonTokenStream(new JavaLexer(CharStreams.fromFileName("D:\\rm2pt\\cocome-hyperledger\\src\\main\\java\\entities\\Store.java")));
+		var parser = new JavaParser(tokens);
+		TokenStreamRewriter rewriter = new TokenStreamRewriter(tokens);
+
+		EntityConverter converter = new EntityConverter(tokens, rewriter);
+
+		converter.visit(parser.compilationUnit());
+
+		System.out.println(rewriter.getText());
+
+	}
+}
